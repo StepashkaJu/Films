986368b891ef4685711c39594512c7c26deedf7d
StepashkaJu <yuliyanosova0807@gmail.com>
2023-11-20 15:59:18 +0400

    style header

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,9 @@
 function Header () { //т.к. у header-a нет никаких состояний, то делаем его функциональным компонентом, не классовым
- return   <nav>
+ return   <nav className="deep-purple lighten-3"> 
  <div className="nav-wrapper">
    <a href="#" className="brand-logo">React Films</a>
    <ul id="nav-mobile" className="right hide-on-med-and-down">
-     <li><a href="collapsible.html">JavaScript</a></li>
+     <li><a href="!#">Smth</a></li>
    </ul>
  </div>
 </nav>
