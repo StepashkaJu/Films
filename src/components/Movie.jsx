94f3e4b08fdc91f1158da0c9e39a52a9cdb3e1f7
StepashkaJu <yuliyanosova0807@gmail.com>
2023-11-22 16:43:11 +0400

    add style

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -9,7 +9,10 @@ function Movie(props) {
   return (
     <div id ={id} className="card movie">
       <div className="card-image waves-effect waves-block waves-light">
-        <img className="activator" src={poster} alt="movie poster" />
+        {
+            poster ==='N/A' ? <img className="activator" src={`"https://via.placeholder.com/200x80/258DC8/E0F6FD?text=${title}"`} alt="movie poster" /> : <img className="activator" src={poster} alt="movie poster" />
+        }
+        
       </div>
       <div className="card-content">
         <span className="card-title activator grey-text text-darken-4">
