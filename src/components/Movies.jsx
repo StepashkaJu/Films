455f95ece7e6b8a2f87b8354ab4ec0ce134e1b00
StepashkaJu <yuliyanosova0807@gmail.com>
2023-11-22 16:07:48 +0400

    add Movies

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,5 +1,18 @@
-function Movies () {
-
+import { Movie } from "./Movie";
+function Movies(props) {
+  return (
+    <div className="movies">
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          year={movie.year}
+          type={movie.type}
+          poster={movie.poster}
+        /> //либо после key добавить спред-оператор {...movie}, И он сам подтянет остальные props
+      ))}
+    </div>
+  );
 }
 
-export {Movies}
+export { Movies };
