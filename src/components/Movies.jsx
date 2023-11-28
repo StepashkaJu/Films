a6ea95e530adf849fd37a44d2607891aee0e3d91
StepashkaJu <yuliyanosova0807@gmail.com>
2023-11-28 19:05:12 +0400

    catch errors

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,13 +1,15 @@
 import { Movie } from "./Movie";
 function Movies(props) {
+  const { movies = [] } = props;
   return (
     <div className="movies">
-      {props.movies.map((movie) => (
-        <Movie
-          key={movie.id}
-          {...movie}
-        /> // после key спред-оператор {...movie}, он сам подтянет остальные props
-      ))}
+      {movies.length ? (
+        props.movies.map((movie) => (
+          <Movie key={movie.id} {...movie} /> // после key спред-оператор {...movie}, он сам подтянет остальные props
+        ))
+      ) : (
+        <h3>Nothing found</h3>
+      )}
     </div>
   );
 }
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -5,27 +5,28 @@ import { Search } from "../components/Search";
 class Main extends React.Component {
   state = {
     movies: [],
+    loading: true, //идет загрузка
   };
   componentDidMount() {
     fetch(`http://www.omdbapi.com/?apikey=6bb8a9b6&s=matrix`)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
+      .then((data) => this.setState({ movies: data.Search, loading: false})); //все загрузилось
   }
 
   searchMovies = (str, type = "all") => {
     fetch(`http://www.omdbapi.com/?apikey=6bb8a9b6&s=${str}${type !== 'all' ? `&type=${type}` : ""}`) // если у нас выбрат тип вильмов не all, то в запрос добавится Get параметр c type=тот тип, который мы выбираем по радиокнопке и отобразятся нужные фильмы или сериалы, если выбран all, то в запрос просто добавится пустая строка,т.к. all выбран по умолчанию type="all"
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search }));
+      .then((data) => this.setState({ movies: data.Search, loading: false }));//все загрузилось
   };
 
   render() {
     return (
       <main className="container-content">
         <Search searchMovies={this.searchMovies} />
-        {this.state.movies.length ? (
-          <Movies movies={this.state.movies} />
-        ) : (
+        {this.state.loading ? (
           <Preloader />
+        ) : (
+          <Movies movies={this.state.movies} />
         )}
       </main>
     );
