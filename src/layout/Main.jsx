75e53f8ba4d8c4f4d0fba93a70fc4e19a95d5d59
StepashkaJu <yuliyanosova0807@gmail.com>
2023-11-29 16:29:51 +0400

    add env.local for API_KEY

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import { Movies } from "../components/Movies";
 import { Preloader } from "../components/Preloader";
 import { Search } from "../components/Search";
+const API_KEY = process.env.REACT_APP_KEY;
 class Main extends React.Component {
   state = {
     movies: [],
     loading: true, //идет загрузка
   };
+
+  
   componentDidMount() {
-    fetch(`http://www.omdbapi.com/?apikey=6bb8a9b6&s=matrix`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false})); //все загрузилось
   }
 
   searchMovies = (str, type = "all") => {
-    fetch(`http://www.omdbapi.com/?apikey=6bb8a9b6&s=${str}${type !== 'all' ? `&type=${type}` : ""}`) // если у нас выбрат тип вильмов не all, то в запрос добавится Get параметр c type=тот тип, который мы выбираем по радиокнопке и отобразятся нужные фильмы или сериалы, если выбран all, то в запрос просто добавится пустая строка,т.к. all выбран по умолчанию type="all"
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ""}`) // если у нас выбрат тип вильмов не all, то в запрос добавится Get параметр c type=тот тип, который мы выбираем по радиокнопке и отобразятся нужные фильмы или сериалы, если выбран all, то в запрос просто добавится пустая строка,т.к. all выбран по умолчанию type="all"
       .then((response) => response.json())
       .then((data) => this.setState({ movies: data.Search, loading: false }));//все загрузилось
   };
